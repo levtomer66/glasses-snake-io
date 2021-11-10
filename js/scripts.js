e88219ac4e0da6cb57406808dbbcd2639b818c0a
Tomer Lev <tomer.l@lusha.com>
2021-11-10 20:39:02 +0200

    small is a real HTML tag, who knew.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function scream() {
-    alert("hi")
-    
+function play() {
+    var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+    audio.play();
 }
