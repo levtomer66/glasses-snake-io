bf9607b6288c6d85763036bd6f55a4d9f62cbfc0
Tomer Lev <tomer.l@lusha.com>
2021-11-11 00:08:51 +0200

    I already said I was sorry

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 function play() {
-    var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+    var audio = new Audio('assets/koshfir.mp3');
     audio.play();
 }
